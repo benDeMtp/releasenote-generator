5001bdef73723c8eb67980163bbb61c40c6232a8
Benjamin RAIMONDI <benjamin.raimondi-prestataire@laposte.fr>
2024-09-03 16:13:03 +0200

    feat : Generate a release note in asciidoc format.
    If the commits follow the conventional commits, the release note will be well formatted

---
+++ src/main/java/com/kawamind/ReleaseNoteGenerator.java
@@ -0,0 +1,343 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17
+//REPO mavencentral
+//DEPS info.picocli:picocli:4.6.3
+//DEPS org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r
+//DEPS org.slf4j:slf4j-simple:2.0.11
+package com.kawamind;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+@Command(name = "releaseNoteGenerator", mixinStandardHelpOptions = true, version = "releaseNoteGenerator 0.1",
+        description = "generate releaseNote for conventional commits")
+public class ReleaseNoteGenerator implements Callable<Integer> {
+
+    SimpleDateFormat spf = new SimpleDateFormat("dd/MM/yyyy");
+
+
+    @Option(description = "local git repository",names = {"-d","--directory"})
+    private String target;
+
+    @Option(description = "file relative path where the release note will be writen",names = {"-o","--output"}, defaultValue = "release-note.adoc")
+    private String output;
+
+    @Option(description = "Tag name", names = {"-t","--tags"})
+    private String tag;
+
+
+    @CommandLine.ArgGroup(exclusive = false)
+    BugTracker bugTracker;
+
+
+    static class BugTracker {
+        @Option(description = "Issue id pattern (regex)", names = {"-p","--pattern"},required = true)
+        private String issueIdPattern;
+
+        @Option(description = "Bug traker url", names = {"-b","--bug-tracker-url"},required = true)
+        private String url;
+    }
+
+    Pattern issueKey;
+
+    private static int DEFAULT_MAX_VERSION = 5;
+
+
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new ReleaseNoteGenerator()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        if(Objects.isNull(target))
+            target = System.getProperty("user.dir");
+        Path p = Path.of(target);
+
+        if(!Objects.isNull(bugTracker) && !Objects.isNull(bugTracker.issueIdPattern)){
+            issueKey = Pattern.compile(bugTracker.issueIdPattern,Pattern.CASE_INSENSITIVE);
+        }
+
+        var git = Git.open(p.toFile());
+        var reached = new AtomicBoolean(Boolean.FALSE);
+
+        List<Ref> allTags = new ArrayList<>(git.tagList().call().stream().toList());
+        allTags.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
+
+        var firstTagReached = new AtomicBoolean(Boolean.FALSE);
+        var givenTagReached = new AtomicBoolean(Boolean.FALSE);
+        try(var releasenote = new PrintWriter(new FileWriter(p.resolve(output).toFile(),false))){
+            releasenote.println("""
+                ==  Releases Note
+                :toc:
+                :toc-title: Versions récentes
+                    
+                """);
+
+            Iterable<RevCommit> commits = null;
+            final List<Ref> tagsList = allTags.stream().toList();
+            try {
+                commits = git.log().call();
+            }catch (NoHeadException e){
+                System.err.println("There is no commit in the repository");
+                return 1;
+            }
+            var listedTag = new AtomicInteger(0);
+            System.out.println("Tag existants : "+tagsList.size());
+
+            List<ReleaseNoteForVersion> versions2commit = new ArrayList<>();
+            AtomicReference<ReleasedVersion> currentVersion = new AtomicReference<>();
+            AtomicInteger currentVersionId = new AtomicInteger();
+
+            commits.forEach(currentCommit -> {
+                if(tagsList.stream().anyMatch(tg -> hasTagMatchingCommit(tg, currentCommit,git.getRepository()))){//Check if the commit have an tag
+                    if(!versions2commit.isEmpty()){
+                        currentVersionId.getAndIncrement();
+                    }
+                    String version = tagsList.stream().filter(tg->hasTagMatchingCommit(tg, currentCommit,git.getRepository())).findFirst().get().getName().replace("refs/tags/", "");
+                    currentVersion.set(new ReleasedVersion(version, spf.format(currentCommit.getAuthorIdent().getWhen())));
+                    versions2commit.add(new ReleaseNoteForVersion(currentVersion.get(), new ArrayList<>()));
+                }
+
+                if(!versions2commit.isEmpty() && messageFilter.test(currentCommit.getFullMessage()) && !currentCommit.getFullMessage().startsWith("[dev]"))
+                    versions2commit.get(currentVersionId.get()).commits.add(currentCommit);
+            });
+
+
+            String typePattern = "^((build|fix|docs|doc|feat|refactor|style|test|chore|ops|perf)?\\(?([a-zA-Z0-9\\-\\s]*)\\)?:?(.*))$";
+            final Pattern pattern = Pattern.compile(typePattern);
+            final List<ToDisplay> versionToDisplay = new ArrayList<>();
+            versions2commit.forEach((rnfv) -> {
+                final Map<String,List<String>> commitsByType = new HashMap<>();
+                rnfv.commits.forEach(commit -> {
+                    var m = pattern.matcher(commit.getShortMessage());
+                    if(m.find()){
+                        var type = m.group(2)!=null? m.group(2):"chore";
+                        var precision = m.group(3);
+                        var commitmessage = type!=null?m.group(4):(m.group(3)!=null?m.group(3):"");
+
+
+                        if(!commitsByType.containsKey(type)){
+                            commitsByType.put(type, new ArrayList<>());
+                        }
+                        try{
+                            Supplier<String> commitMessageSupplier = ()-> commitmessage.trim().isEmpty()?"":handleCommitMessage(commitmessage).trim();
+                            Supplier<String> commitMessageSuplier2 = () -> (((precision!=null && !precision.trim().isEmpty())?(handleCommitMessage(precision).trim()+" : "):"")+commitMessageSupplier.get()).trim();
+                            if(commitMessageSuplier2.get()!=null && !commitMessageSuplier2.get().isEmpty()){
+                                commitsByType.get(type).add(commitMessageSuplier2.get());
+                            }else{
+                                System.out.println("commitMessageSupplier "+commitMessageSupplier.get());
+                                System.out.println("commitMessageSupplier2 "+commitMessageSuplier2.get());
+                                System.out.println(commit.getShortMessage());
+                                System.out.println(type+" / '"+precision+"' / "+commitmessage);
+                            }
+                        } catch (Exception e){
+                            System.out.println("there is an issue with "+ commit.getShortMessage());
+                            System.out.println(type+" "+precision+" "+commitmessage);
+                        }
+
+
+                    }
+                });
+                versionToDisplay.add(new ToDisplay(rnfv, commitsByType));
+            });
+
+
+
+            versionToDisplay.forEach((td) -> {
+                if(firstTagReached.get()){
+                    releasenote.println("");
+                }
+
+                if(!givenTagReached.get() && (isPoinsonPill.apply(tag, td.releaseNoteForVersion.releasedVersion.version) || (Objects.isNull(tag) && listedTag.get()>=DEFAULT_MAX_VERSION))){//View : if the condition is true, switch to historic mode
+                    releasenote.println("""
+                    .Anciennes versions    
+                    [%collapsible]
+                    ====
+                        """);
+                    givenTagReached.set(Boolean.TRUE);
+                }
+                releasenote.println(versionStringAdoc(td.releaseNoteForVersion.releasedVersion.version,td.releaseNoteForVersion.releasedVersion.date,givenTagReached.get()));//display the version number
+                firstTagReached.set(Boolean.TRUE);
+                listedTag.getAndIncrement();
+
+                if(td.types.containsKey("feat")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Features 💰");
+                    td.types.get("feat").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("fix")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Corrections 🩹");
+                    td.types.get("fix").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("refactor") || td.types.containsKey("perf")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Refactorings 🚀");
+                    if(td.types.containsKey("refactor"))
+                        td.types.get("refactor").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    if(td.types.containsKey("perf"))
+                        td.types.get("perf").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("build")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Dépendances, versions, CI, etc. ⚙️");
+                    td.types.get("build").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("ops")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Infra, déploiement, etc. 📡");
+                    td.types.get("ops").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("styled")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Style ✂️");
+                    td.types.get("style").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("doc") || td.types.containsKey("docs")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Documentation 📜");
+                    if(td.types.containsKey("doc"))
+                        td.types.get("doc").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    if(td.types.containsKey("docs"))
+                        td.types.get("docs").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+                if(td.types.containsKey("chore")){
+                    releasenote.println(typeStringAdoc(firstTagReached.get())+"Divers");
+                    td.types.get("chore").stream().forEach(t -> releasenote.println(commitStringAdoc(firstTagReached.get())+t));
+                    releasenote.println();
+                }
+
+            });
+
+
+            //fin de la release note
+            if(givenTagReached.get()){
+                releasenote.println("====");
+            }
+            releasenote.close();
+        }
+        return 0;
+    }
+
+    Boolean hasTagMatchingCommit(Ref t,RevCommit u,Repository repo ) {
+        return getActualRefObjectId(t,repo).equals(u.getId());
+
+    };
+
+    private ObjectId getActualRefObjectId(Ref ref,Repository repo) {
+        final Ref repoPeeled = repo.peel(ref);
+        if(repoPeeled.getPeeledObjectId() != null) {
+            return repoPeeled.getPeeledObjectId();
+        }
+        return ref.getObjectId();
+    }
+
+    Pattern oldReleasePattern = Pattern.compile("^release\\s\\d(\\.\\d{1,3}){1,2}.*$");
+
+    String[] filteredMessage = {"[release]","nouvelle version","Merge branch","releasenote","prepare next dev version"};
+
+    Predicate<String> messageFilter = new Predicate<String>() {
+        @Override
+        public boolean test(String messageToTest) {
+
+            if(Arrays.asList(filteredMessage).stream().anyMatch(m -> messageToTest.contains(m))){
+                return false;
+            }else{
+
+                var m = oldReleasePattern.matcher(messageToTest.trim());
+                if(m.matches()){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    };
+
+    BiFunction<String,String,Boolean> isPoinsonPill = new BiFunction<String,String,Boolean>() {
+        @Override
+        public Boolean apply(String givenTag, String currentTag) {
+            if(Objects.isNull(givenTag) || givenTag.isBlank()){
+                return Boolean.FALSE;
+            }else{
+                if(givenTag.equals(currentTag.replace("refs/tags/", "")))
+                    return Boolean.TRUE;
+            }
+            return Boolean.FALSE;
+        }
+    };
+
+    String handleCommitMessage(String message){
+        if(issueKey!=null && bugTracker.url!=null) {
+            var m = issueKey.matcher(message);
+            if (m.find()) {
+                return m.replaceFirst("link:" + bugTracker.url + "$0[$0]");
+            }
+        }
+        return message;
+    }
+
+
+
+    String versionStringAdoc(String version,String date,boolean collapsibleBlock){
+        return (collapsibleBlock?"":"=== ")+"Version : "+version+" ("+date+")"+(collapsibleBlock?"\n":"");
+    }
+
+    String typeStringAdoc(boolean collapsibleBlock){
+        return (collapsibleBlock?"* ":"==== ");
+    }
+
+    String commitStringAdoc(boolean collapsibleBlock){
+        return (collapsibleBlock?"** ":"- ");
+    }
+
+    public record ReleasedVersion(String version, String date) {
+    }
+
+    public record ReleaseNoteForVersion(ReleasedVersion releasedVersion, List<RevCommit> commits){}
+
+    public record ToDisplay(ReleaseNoteForVersion releaseNoteForVersion,Map<String,List<String>> types){}
+    /*
+     * API relevant changes
+
+    feat Commits, that adds or remove a new feature
+    fix Commits, that fixes a bug
+
+refactor Commits, that rewrite/restructure your code, however does not change any API behaviour
+
+    perf Commits are special refactor commits, that improve performance
+
+style Commits, that do not affect the meaning (white-space, formatting, missing semi-colons, etc)
+test Commits, that add missing tests or correcting existing tests
+docs Commits, that affect documentation only
+build Commits, that affect build components like build tool, ci pipeline, dependencies, project version, ...
+ops Commits, that affect operational components like infrastructure, deployment, backup, recovery, ...
+chore Miscellaneou
+     *
+     */
+
+
+}
---
+++ src/test/java/com/kawamind/ReleaseNoteGeneratorTest.java
@@ -0,0 +1,230 @@
+package com.kawamind;
+
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainLauncher;
+import io.quarkus.test.junit.main.QuarkusMainTest;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.SoftAssertions;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.TagCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusMainTest
+@Slf4j
+class ReleaseNoteGeneratorTest {
+
+    Repository tmpRepo;
+    String tmpRepoPath;
+
+    PersonIdent personIdent = new PersonIdent("Bill","bill@ggamil.foo",LocalDate.parse("2018-05-05").atStartOfDay().toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
+
+    @BeforeEach
+    void setUp() throws IOException, GitAPIException {
+        tmpRepo = createNewRepository();
+        tmpRepoPath = tmpRepo.getDirectory().getAbsolutePath();
+    }
+
+    void addFileAndCommit(String message) throws IOException, GitAPIException {
+        Git git = new Git(tmpRepo);
+        AddCommand add = git.add();
+        var path = tmpRepo.getDirectory().toPath();
+        var fileName = UUID.randomUUID() +".html";
+        var file1 = Files.createFile(path.resolve(fileName));
+        add.addFilepattern(file1.getFileName().toString()).call();
+        CommitCommand commit = git.commit();
+        commit.setAuthor(personIdent).setMessage(message).call();
+    }
+
+
+
+    void addTag(String name) throws GitAPIException {
+        Git git = new Git(tmpRepo);
+        TagCommand tag = git.tag();
+        tag.setTagger(personIdent).setName(name).setForceUpdate(true).call();
+    }
+
+    @Test
+    @DisplayName("ReleaseNoteGenerator should fail with message if there is no commit")
+    void rngShouldFailWithMessageIfThereIsNoCommit(QuarkusMainLauncher launcher){
+        LaunchResult result =launcher.launch("-d",tmpRepoPath);
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(result.exitCode()).as("Status code should be 1").isEqualTo(1);
+        softly.assertThat(result.getErrorOutput()).as("Error output should describe error").isEqualTo("There is no commit in the repository");
+
+        softly.assertAll();
+    }
+
+    @Test
+    @DisplayName("ReleaseNoteGenerator should create releasenote file a default location")
+    void runReleaseNoteGeneratorShouldCreateReleaseNoteFile(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+        addFileAndCommit("initial commit");
+        LaunchResult result =launcher.launch("-d",tmpRepoPath);
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(result.exitCode()).as("Status code should be 0").isEqualTo(0);
+        softly.assertThat(tmpRepo.getDirectory()).as("Directory should exists").exists();
+        softly.assertThat(new File(tmpRepo.getDirectory().getAbsolutePath(),"release-note.adoc")).as("default releasenote file should exists").exists();
+
+        softly.assertAll();
+
+    }
+
+    @Test
+    @DisplayName("ReleaseNoteGenerator should create releasenote file to specified location")
+    void runReleaseNoteGeneratorShouldCreateReleaseNoteFileToSpecifiedLocation(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+        addFileAndCommit("initial commit");
+        LaunchResult result =launcher.launch("-d",tmpRepoPath,"-o","foo.adoc");
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(result.exitCode()).as("Status code should be 0").isEqualTo(0);
+        softly.assertThat(tmpRepo.getDirectory()).as("Directory should exists").exists();
+        softly.assertThat(new File(tmpRepo.getDirectory().getAbsolutePath(),"foo.adoc")).as("releasenote file should exists").exists();
+
+        softly.assertAll();
+
+    }
+
+    @Test
+    @DisplayName("release note should respect without-convention.adoc")
+    void releaseNoteShouldRespectTemplate1(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+        var standard = new File("src/test/resources/without-convention.adoc");
+        addFileAndCommit("Commit 1");
+        addFileAndCommit("Commit 2");
+        addTag("v1");
+        addFileAndCommit("Commit 3");
+        addFileAndCommit("Commit 4");
+        addTag("v2");
+        var releasenote = tmpRepo.getDirectory().toPath().resolve("release-note.adoc").toFile();
+
+        LaunchResult result =launcher.launch("-d",tmpRepoPath);
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode()).as("Status code should be 0").isEqualTo(0);
+        softly.assertThat(releasenote).as("release note should not be empty").isNotEmpty();
+        softly.assertThat(releasenote).as("release note should have the same content as sample file").hasSameTextualContentAs(standard);
+
+        softly.assertAll();
+
+    }
+
+    @Test
+    @DisplayName("release note should respect with-convention.adoc")
+    void releaseNoteShouldRespectTemplateWIthConvention(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+        var standard = new File("src/test/resources/with-convention.adoc");
+        addFileAndCommit("fix: issue1");
+        addFileAndCommit("fix: issue2");
+        addFileAndCommit("feat: add awesome feature");
+        addTag("v1");
+        addFileAndCommit("test: fix tests");
+        addFileAndCommit("build: add uber dep");
+        addFileAndCommit("build: add uber dep2");
+        addFileAndCommit("style: new UI");
+        addTag("v2");
+        addFileAndCommit("ops: add deploy script");
+        addFileAndCommit("docs: add doc one");
+        addFileAndCommit("doc: add doc two");
+        addFileAndCommit("perf: to the sky");
+        addTag("v3");
+        var releasenote = tmpRepo.getDirectory().toPath().resolve("release-note.adoc").toFile();
+
+        LaunchResult result =launcher.launch("-d",tmpRepoPath);
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(result.exitCode()).as("Status code should be 0").isEqualTo(0);
+        softly.assertThat(releasenote).as("release note should not be empty").isNotEmpty();
+        softly.assertThat(releasenote).as("release note should have the same content as sample file").hasSameTextualContentAs(standard);
+        softly.assertAll();
+
+    }
+
+    @Nested
+    @DisplayName("Tests with bugtracker options")
+    class BugTracker {
+
+        @Test
+        @DisplayName("should failed if -b and -p are not in the command line")
+        void shouldFailedIfDependentOptionsAreNotSet(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+            var standard = new File("src/test/resources/without-convention.adoc");
+            addFileAndCommit("fix(project-1): big error");
+            addFileAndCommit("feat(project-2): awesome feature");
+            addTag("v1");
+            var releasenote = tmpRepo.getDirectory().toPath().resolve("release-note.adoc").toFile();
+
+            LaunchResult result =launcher.launch("-d",tmpRepoPath,"-p","project-\\d*");
+
+            SoftAssertions softly = new SoftAssertions();
+
+            softly.assertThat(result.exitCode()).as("Status code should be 1").isEqualTo(2);
+            softly.assertThat(result.getErrorOutput()).as("Error output should describe error").startsWith("Error: Missing required argument(s): --bug-tracker-url=<url>");
+
+            softly.assertAll();
+
+        }
+
+        @Test
+        @DisplayName("release note should respect without-bugtracker.adoc")
+        void releaseNoteShouldRespectTemplate1(QuarkusMainLauncher launcher) throws GitAPIException, IOException {
+            var standard = new File("src/test/resources/with-bugtracker.adoc");
+            addFileAndCommit("fix(project-1): big error");
+            addFileAndCommit("feat(project-2): awesome feature");
+            addTag("v1");
+            var releasenote = tmpRepo.getDirectory().toPath().resolve("release-note.adoc").toFile();
+
+            LaunchResult result =launcher.launch("-d",tmpRepoPath,"-p","project-\\d*","-b","http://mybugtracker.com/");
+
+            SoftAssertions softly = new SoftAssertions();
+            softly.assertThat(result.exitCode()).as("Status code should be 0").isEqualTo(0);
+            softly.assertThat(releasenote).as("release note should not be empty").isNotEmpty();
+            softly.assertThat(releasenote).as("release note should have the same content as sample file").hasSameTextualContentAs(standard);
+
+            var read  = new BufferedReader(new FileReader(releasenote));
+            read.lines().forEach(line-> System.out.println(line));
+            softly.assertAll();
+
+        }
+
+
+
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        tmpRepo.getDirectory().delete();
+    }
+
+    private static Repository createNewRepository() throws IOException {
+        // prepare a new folder
+        File localPath = File.createTempFile("TestGitRepository", "");
+        if(!localPath.delete()) {
+            throw new IOException("Could not delete temporary file " + localPath);
+        }
+
+        if(!localPath.mkdirs()) {
+            throw new IOException("Could not create directory " + localPath);
+        }
+
+        // create the directory
+        Repository repository = FileRepositoryBuilder.create(new File(localPath, ".git"));
+        repository.create();
+
+        return repository;
+    }
+}
